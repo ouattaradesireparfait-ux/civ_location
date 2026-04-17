e0e9c8749f4c57a5ca69bab4fe2812082271167c
ouattaradesireparfait-ux <ouattaradesireparfait@gmail.com>
2026-04-17 17:20:36 +0000

    ALLOWED_HOST sécurisé

--- civ_location/settings.py
+++ civ_location/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ============================================================
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-civ-location-changez-moi-en-production')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 CSRF_TRUSTED_ORIGINS = [
     'https://cotedivoirelocationdevehicules.onrender.com',
 ]
